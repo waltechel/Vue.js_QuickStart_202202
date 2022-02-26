b8a55aeb68699562b033dbdc6f6000a8e8c17942
dongjun Lee <ldj910505@gmail.com>
2022-02-26 17:18:56 +0900

    11.5 액션

--- examples/ch11/todolistapp2/src/store/index.js
+++ examples/ch11/todolistapp2/src/store/index.js
@@ -5,40 +5,42 @@ import Constant from '../Constant';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state : {
-        todolist : [
-            { id:1, todo : "영화보기", done:false },
-            { id:2, todo : "주말 산책", done:true },
-            { id:3, todo : "ES6 학습", done:false },
-            { id:4, todo : "잠실 야구장", done:false },
+    state: {
+        todolist: [
+            { id: 1, todo: "영화보기", done: false },
+            { id: 2, todo: "주말 산책", done: true },
+            { id: 3, todo: "ES6 학습", done: false },
+            { id: 4, todo: "잠실 야구장", done: false },
         ]
     },
     mutations: {
-        [Constant.ADD_TODO] : (state, payload) => {
+        [Constant.ADD_TODO]: (state, payload) => {
             if (payload.todo !== "") {
                 state.todolist.push(
-                    { id:new Date().getTime(), todo: payload.todo, done:false });
+                    { id: new Date().getTime(), todo: payload.todo, done: false });
             }
         },
-        [Constant.DONE_TOGGLE] : (state, payload) => {
-            var index = state.todolist.findIndex((item)=>item.id === payload.id);
+        [Constant.DONE_TOGGLE]: (state, payload) => {
+            var index = state.todolist.findIndex((item) => item.id === payload.id);
             state.todolist[index].done = !state.todolist[index].done;
         },
-        [Constant.DELETE_TODO] : (state, payload) => {
-            var index = state.todolist.findIndex((item)=>item.id === payload.id);
-            state.todolist.splice(index,1);
+        [Constant.DELETE_TODO]: (state, payload) => {
+            var index = state.todolist.findIndex((item) => item.id === payload.id);
+            state.todolist.splice(index, 1);
         }
     },
-    actions : {
-        [Constant.ADD_TODO] : (store, payload) => {
+    // 변이의 기능에서 외부 리소스 액세스를 비롯한 비동기 처리가 필요한 API 호출을 수행하기 위해
+    // 액션Action을 분리해낸다.
+    actions: {
+        [Constant.ADD_TODO]: (store, payload) => {
             console.log("###addTodo!!!", payload);
             store.commit(Constant.ADD_TODO, payload);
         },
-        [Constant.DELETE_TODO] : (store, payload) => {
+        [Constant.DELETE_TODO]: (store, payload) => {
             console.log("###deleteTodo!!!", payload);
             store.commit(Constant.DELETE_TODO, payload);
         },
-        [Constant.DONE_TOGGLE] : (store, payload) => {
+        [Constant.DONE_TOGGLE]: (store, payload) => {
             console.log("###doneToggle!!!", payload);
             store.commit(Constant.DONE_TOGGLE, payload);
         }
