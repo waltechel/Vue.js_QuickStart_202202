a35f146e01906669598453b6495cc91b3b279004
knecht <ldj910505@naver.com>
2022-02-26 23:25:24 +0900

    12.10 지연 시간에 대한 처리

--- examples/ch12/contactsapp2/src/App.vue
+++ examples/ch12/contactsapp2/src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="container">
-      <div class="page-header">
-         <h1 class="text-center">연락처 관리 애플리케이션</h1>
-         <p>(Vue-router + Vuex + Axios) </p>
-         <div class="btn-group">
-            <router-link to="/home" class="btn btn-info menu">Home</router-link>
-            <router-link to="/about" class="btn btn-info menu">About</router-link>
-            <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
-         </div>
+    <div class="page-header">
+      <h1 class="text-center">연락처 관리 애플리케이션</h1>
+      <p>(Vue-router + Vuex + Axios)</p>
+      <div class="btn-group">
+        <router-link to="/home" class="btn btn-info menu">Home</router-link>
+        <router-link to="/about" class="btn btn-info menu">About</router-link>
+        <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
       </div>
-      <router-view></router-view>
-      <loading v-show="isloading"></loading>
+    </div>
+    <router-view></router-view>
+    <loading v-show="isloading"></loading>
   </div>
 </template>
 
 <script>
-import Loading from './components/Loading';
-import { mapState } from 'vuex';
+import Loading from "./components/Loading";
+import { mapState } from "vuex";
 
-export default {    
-  name: 'app',
-  components : { Loading },
-  computed : mapState([ 'isloading' ])
-}
+export default {
+  name: "app",
+  components: { Loading },
+  computed: mapState(["isloading"])
+};
 </script>
 
 <style scoped>
 #container {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -35,6 +35,6 @@ export default {
   margin-top: 60px;
 }
 .menu {
-  width : 100px;
+  width: 100px;
 }
 </style>
