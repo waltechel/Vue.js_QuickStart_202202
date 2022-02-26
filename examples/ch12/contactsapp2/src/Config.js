ac3c40e92bfdac1df2a8df20485d5c44262f175f
knecht <ldj910505@naver.com>
2022-02-26 23:25:08 +0900

    12.10 지연 시간에 대한 처리

--- examples/ch12/contactsapp2/src/Config.js
+++ examples/ch12/contactsapp2/src/Config.js
@@ -1,18 +1,19 @@
 var BASE_URL = "/api";
 
 export default {
-    PAGESIZE : 5,
-    
+    PAGESIZE: 5,
+
+    // 지연 시간의 발생 동안 처리 중임을 알릴 수 있는 UI가 제공되어야 한다.
     //전체 연락처 데이터 요청(페이징 포함)
-    FETCH : BASE_URL + "/contacts_long",
+    FETCH: BASE_URL + "/contacts_long",
     //연락처 추가
-    ADD : BASE_URL + "/contacts",
+    ADD: BASE_URL + "/contacts",
     //연락처 업데이트
-    UPDATE : BASE_URL + "/contacts/${no}",
+    UPDATE: BASE_URL + "/contacts/${no}",
     //연락처 한건 조회
-    FETCH_ONE : BASE_URL + "/contacts/${no}",
+    FETCH_ONE: BASE_URL + "/contacts/${no}",
     //연락처 삭제
-    DELETE : BASE_URL + "/contacts/${no}",
+    DELETE: BASE_URL + "/contacts/${no}",
     //연락처 사진 업로드->변경
-    UPDATE_PHOTO : BASE_URL + "/contacts/${no}/photo"
+    UPDATE_PHOTO: BASE_URL + "/contacts/${no}/photo"
 }
--- examples/ch12/contactsapp2/src/store/actions.js
+++ examples/ch12/contactsapp2/src/store/actions.js
@@ -3,75 +3,75 @@ import axios from 'axios';
 import CONF from '../Config'
 
 export default {
-    [Constant.CHANGE_ISLOADING] : (store, payload) => {
+    [Constant.CHANGE_ISLOADING]: (store, payload) => {
         store.commit(Constant.CHANGE_ISLOADING, payload);
     },
-    [Constant.FETCH_CONTACTS] : (store, payload) => {
-        var pageno;
-        if (typeof payload ==="undefined" || typeof payload.pageno ==="undefined")
+    [Constant.FETCH_CONTACTS]: (store, payload) => {
+        let pageno;
+        if (typeof payload === "undefined" || typeof payload.pageno === "undefined")
             pageno = 1;
-        else 
+        else
             pageno = payload.pageno;
-        var pagesize = store.state.contactlist.pagesize;
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
+        let pagesize = store.state.contactlist.pagesize;
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
         axios.get(CONF.FETCH, {
-            params : { pageno: pageno, pagesize: pagesize } 
-        }).then((response)=> {
+            params: { pageno: pageno, pagesize: pagesize }
+        }).then((response) => {
             store.commit(Constant.FETCH_CONTACTS, { contactlist: response.data });
-            store.dispatch(Constant.CHANGE_ISLOADING, { isloading:false });
+            store.dispatch(Constant.CHANGE_ISLOADING, { isloading: false });
         })
     },
-    [Constant.ADD_CONTACT] : (store) => {
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
-        axios.post(CONF.ADD,  store.state.contact)
-        .then((response)=> {
-            if (response.data.status == "success") {
-                store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1});                
-            } else {
-                console.log("연락처 추가 실패 : " + response.data);
-            }
-        })
+    [Constant.ADD_CONTACT]: (store) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+        axios.post(CONF.ADD, store.state.contact)
+            .then((response) => {
+                if (response.data.status === "success") {
+                    store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1 });
+                } else {
+                    console.log("연락처 추가 실패 : " + response.data);
+                }
+            })
     },
-    [Constant.UPDATE_CONTACT] : (store) => {
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
-        var currentPageNo = store.state.contactlist.pageno;
-        var contact = store.state.contact;
+    [Constant.UPDATE_CONTACT]: (store) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+        let currentPageNo = store.state.contactlist.pageno;
+        let contact = store.state.contact;
         axios.put(CONF.UPDATE.replace("${no}", contact.no), contact)
-        .then((response)=> {
-            if (response.data.status == "success") {
-                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-            } else {
-                console.log("연락처 변경 실패 : " + response.data)
-            }
-        })
+            .then((response) => {
+                if (response.data.status === "success") {
+                    store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+                } else {
+                    console.log("연락처 변경 실패 : " + response.data)
+                }
+            })
     },
-    [Constant.UPDATE_PHOTO] : (store, payload) => {
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
-        var currentPageNo = store.state.contactlist.pageno;
-        var data = new FormData();
+    [Constant.UPDATE_PHOTO]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+        let currentPageNo = store.state.contactlist.pageno;
+        let data = new FormData();
         data.append('photo', payload.file);
         axios.post(CONF.UPDATE_PHOTO.replace("${no}", payload.no), data)
-        .then(()=> {
-            store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-        })
+            .then(() => {
+                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+            })
     },
-    [Constant.DELETE_CONTACT] : (store, payload)=> {
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
-        var currentPageNo = store.state.contactlist.pageno;
+    [Constant.DELETE_CONTACT]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
+        let currentPageNo = store.state.contactlist.pageno;
         axios.delete(CONF.DELETE.replace("${no}", payload.no))
-        .then(()=> {
-            store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-        })
+            .then(() => {
+                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+            })
     },
-    [Constant.FETCH_CONTACT_ONE] : (store, payload) => {
-        store.dispatch(Constant.CHANGE_ISLOADING, { isloading:true });
+    [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
+        store.dispatch(Constant.CHANGE_ISLOADING, { isloading: true });
         axios.get(CONF.FETCH_ONE.replace("${no}", payload.no))
-        .then((response)=> {
-            store.commit(Constant.FETCH_CONTACT_ONE, { contact:response.data });
-            store.dispatch(Constant.CHANGE_ISLOADING, { isloading:false });
-        })
+            .then((response) => {
+                store.commit(Constant.FETCH_CONTACT_ONE, { contact: response.data });
+                store.dispatch(Constant.CHANGE_ISLOADING, { isloading: false });
+            })
     },
-    [Constant.INITIALIZE_CONTACT_ONE] : (store) => {
+    [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
         store.commit(Constant.INITIALIZE_CONTACT_ONE);
     }
 }
