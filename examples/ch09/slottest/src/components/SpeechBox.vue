ca8238f17bf6e9bc279e1eac046654c3ff5f31e7
dongjun Lee <ldj910505@gmail.com>
2022-02-26 14:47:04 +0900

    9.3 슬롯

--- examples/ch09/slottest/src/components/SpeechBox.vue
+++ examples/ch09/slottest/src/components/SpeechBox.vue
@@ -2,7 +2,8 @@
   <div class="container">
     <div class="header">{{headerText}}</div>
     <div class="content">
-        <slot></slot>
+      <!-- 슬롯을 이용해 부모 컴포넌트에서 자식 컴포넌트로 HTML 마크업을 전달할 수 있다. -->
+      <slot></slot>
     </div>
     <div class="footer">{{footerText}}</div>
   </div>
@@ -10,17 +11,33 @@
 
 <script>
 export default {
-    props : [ 'headerText', 'footerText' ]
-}
+  props: ["headerText", "footerText"]
+};
 </script>
 
 <style scoped>
-.container { width:300px; margin:10px; padding:2px;
-    border:solid 1px gray; float:left; }
-.header { padding:4px 20px 4px 20px; background-color: orange;
-    color:aqua; text-align: center; }
-.footer { padding:4px 20px 4px 20px; background-color: aqua; 
-    text-align:left; }
-.content { padding: 10px; height:auto; min-height: 40px; 
-    text-align:left; }
+.container {
+  width: 300px;
+  margin: 10px;
+  padding: 2px;
+  border: solid 1px gray;
+  float: left;
+}
+.header {
+  padding: 4px 20px 4px 20px;
+  background-color: orange;
+  color: aqua;
+  text-align: center;
+}
+.footer {
+  padding: 4px 20px 4px 20px;
+  background-color: aqua;
+  text-align: left;
+}
+.content {
+  padding: 10px;
+  height: auto;
+  min-height: 40px;
+  text-align: left;
+}
 </style>
--- examples/ch09/slottest/src/main.js
+++ examples/ch09/slottest/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
+// 슬롯 예제를 바꾸기 위한 곳
 //import App from './App.vue'
-import App from './AppNamed.vue'
-//import App from './AppScoped.vue'
+//import App from './AppNamed.vue'
+import App from './AppScoped.vue'
 
 Vue.config.productionTip = false
 
