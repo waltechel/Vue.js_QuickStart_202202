f5b036340cbae3c0a0094d09f76e530660c09d82
dongjun Lee <ldj910505@gmail.com>
2022-02-26 14:58:53 +0900

    9.5 재귀 컴포넌트

--- examples/ch09/dynamictest/src/components/Tree.vue
+++ examples/ch09/dynamictest/src/components/Tree.vue
@@ -1,15 +1,17 @@
 <template>
+  <!-- 재귀 컴포넌트 : 템플릿에서 자기 자신을 호출하는 컴포넌트 -->
   <ul>
     <li v-for="s in subs" v-bind:class="s.type" :key="s.name">
-        {{s.name}}
-        <tree :subs="s.subs"></tree>
-    </li>    
+      {{s.name}}
+      <!-- 재귀 참조를 위해서 tree 컴포넌트에서 tree 컴포넌트를 활용합니다. -->
+      <tree :subs="s.subs"></tree>
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-    name : 'tree',
-    props : [ 'subs' ]
-}
+  name: "tree",
+  props: ["subs"]
+};
 </script>
