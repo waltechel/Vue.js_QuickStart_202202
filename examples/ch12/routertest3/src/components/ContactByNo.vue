684fa355d88ae2ee4ea44fe97e695cb5c381aeb3
dongjun Lee <ldj910505@gmail.com>
2022-02-26 18:35:58 +0900

    12.4 중첩 라우트

--- examples/ch12/routertest3/src/components/ContactByNo.vue
+++ examples/ch12/routertest3/src/components/ContactByNo.vue
@@ -1,59 +1,68 @@
 <template>
   <div>
     <hr class="divider" />
-    <div >
-        <table class="detail table table-bordered">
-            <tbody>
-            <tr class="active">
-                <td>일련번호</td>
-                <td>{{contact.no}}</td>
-            </tr>
-            <tr class="active">
-                <td>이름</td>
-                <td>{{contact.name}}</td>
-            </tr>
-            <tr class="active">
-                <td>전화</td>
-                <td>{{contact.tel}}</td>
-            </tr>
-            <tr class="active">
-                <td>주소</td>
-                <td>{{contact.address}}</td>
-            </tr>
-            </tbody>
-        </table>
+    <div>
+      <table class="detail table table-bordered">
+        <tbody>
+          <tr class="active">
+            <td>일련번호</td>
+            <td>{{contact.no}}</td>
+          </tr>
+          <tr class="active">
+            <td>이름</td>
+            <td>{{contact.name}}</td>
+          </tr>
+          <tr class="active">
+            <td>전화</td>
+            <td>{{contact.tel}}</td>
+          </tr>
+          <tr class="active">
+            <td>주소</td>
+            <td>{{contact.address}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import contactlist from '../ContactList';
+import contactlist from "../ContactList";
 export default {
-    name : 'contactbyno',
-    data : function() {
-        return {
-            no : 0,
-            contacts : contactlist.contacts
-        }
-    },
-    created : function() {
-        this.no = parseInt(this.$route.params.no, 10);
-    },
-    watch : {
-        '$route'(to) {
-            this.no = parseInt(to.params.no, 10);
-        }
-    },
-    computed : {
-        contact : function() {
-            return this.contacts.find((item)=>item.no ===this.no)
-        }
+  name: "contactbyno",
+  data() {
+    return {
+      no: 0,
+      contacts: contactlist.contacts
+    };
+  },
+  created() {
+    this.no = parseInt(this.$route.params.no, 10);
+  },
+  watch: {
+    // $route 속성에 대해 관찰을 수행합니다.
+    $route(to) {
+      this.no = parseInt(to.params.no, 10);
     }
-}
+  },
+  computed: {
+    contact() {
+      return this.contacts.find(item => item.no === this.no);
+    }
+  }
+};
 </script>
 
 <style>
-.divider { height: 3px; margin-left: auto;margin-right: auto;
-    background-color:#FF0066; color:#FF0066; border: 0 none; }
-table.detail { width:400px; }
+.divider {
+  height: 3px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #ff0066;
+  color: #ff0066;
+  border: 0 none;
+}
+table.detail {
+  width: 400px;
+}
 </style>
--- examples/ch12/routertest3/src/components/Home.vue
+++ examples/ch12/routertest3/src/components/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-      <h1>Home</h1>
+    <h1>Home</h1>
   </div>
 </template>
 <script>
 export default {
-  name : "home"
-}
+  name: "home"
+};
 </script>
